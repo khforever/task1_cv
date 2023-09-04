de9aa7fce149bd794a44d2080ba1c622353f8ae0
khforever <59407743+khforever@users.noreply.github.com>
2023-09-04 13:05:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+
+ 
+
+
+
+
+<?php  echo "  kholoud mohamed cv Template";  ?>
+
+
+ 
+ 
+ </title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="https://cdn-icons-png.flaticon.com/512/3220/3220315.png" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+            <h2><?php  echo "kholoud Mohamed Hafez";  ?></h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>
+          
+          
+          
+          
+          <?php  echo "PHP Devolper";  ?>
+          
+          
+ 
+          
+          
+        </p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>
+          
+          
+          <?php  echo "Cairo ,Egypt";  ?>
+           </p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>
+          
+          <?php  echo "eng.kholoumohamed@gmail.com";  ?>
+          
+        
+        
+        </p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>
+          
+          <?php  echo "01147043910";  ?>
+        
+        </p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>
+          
+          
+          
+          
+          
+          <?php  echo " Skills";  ?>
+          
+    </b></p>
+          <p>
+              
+          <?php  echo "PHP skills";  ?>
+          </p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>
+              
+           
+         
+          <?php  echo "My SQL skills";  ?>
+        
+        </p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <p>
+              
+           
+          <?php  echo "OOP";  ?>
+          
+          </p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:75%">75%</div>
+          </div>
+          <p>
+              
+           
+          <?php  echo "laravel";  ?>
+          
+        </p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:50%">50%</div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>
+          
+          
+          <?php  echo "Languaged  ";  ?>
+    </b></p>
+          <p>
+              
+          <?php  echo "Arabic";  ?>
+          </p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
+          </div>
+          <p>
+          <?php  echo "English";  ?> 
+          
+           </p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:55%"></div>
+          </div>
+          <p>
+              
+          <?php  echo "German";  ?>
+          
+          </p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:25%"></div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+
+      
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>
+        
+        
+        <?php  echo " Work Experience";  ?>
+        
+      
+        
+        </h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>
+              
+          <?php  echo "PHP devolper / EgyptWeb company";  ?>
+          
+          
+        
+        
+        </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>
+          
+          
+          <?php  echo "Dec  2020 - ";  ?>
+ 
+          
+          
+          
+          <span class="w3-tag w3-teal w3-round">
+              
+          
+          <?php  echo "Current";  ?>
+          
+          </span></h6>
+          <p>
+              
+          <?php  echo " Acess php script and modify tasks for php script";  ?>
+           
+          
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>
+              
+          
+          <?php  echo "Wordpress devolper / Future company";  ?>
+          
+        
+        
+        
+        
+        </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>
+          
+           
+          <?php  echo " Feb  2020  - Oct 2020";  ?>
+        
+        </h6>
+          <p>
+              
+          
+          
+
+          <?php  echo " create   word press  scripts ";  ?>
+          
+          
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>
+              
+          
+          <?php  echo "Goomla devolper / Dream company";  ?>
+          </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>
+          
+          <?php  echo "Jan 2019 - Jan 2020     ";  ?>
+          
+        </h6>
+          <p>
+              
+          
+          <?php  echo "CURD create,update ,read,delete  scripts";  ?>
+        
+        
+        
+        
+        </p><br>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>
+        
+        
+        <?php  echo "Education ";  ?>
+        
+        
+        </h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>
+              
+          
+          <?php  echo "Faculty of Computer Science";  ?>
+        
+        </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>
+          
+          
+          
+        
+          
+          <?php  echo "2018";  ?>
+          
+          </h6>
+          <p>
+              
+          
+         
+          <?php  echo " The degree is Excellent ";  ?>
+          <?php  echo " <br>";  ?>
+          <?php  echo " <br>";  ?>
+          <?php  echo " The subject is Data strucure  ,Algorithm , OOP";  ?>
+          
+        </p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>
+        
+
+          
+          <?php  echo "El marg secondary School";  ?>
+        
+        
+        </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>
+          
+          
+          
+          <?php  echo "2013  -2015  ";  ?>
+          
+          
+ </h6>
+          <p>
+              
+
+          <?php  echo " The Degree is 95 % ";  ?>
+          </p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>
+              
+          
+                 
+          <?php  echo "  ICDL ";  ?>
+          
+          
+           </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>
+          
+                 
+          <?php  echo " 2010 ";  ?>
+          
+           
+        
+        
+        
+        </h6>
+          <p>
+
+
+       
+          <?php  echo " Word,Powerpoint ,Excel ,Access ";  ?>
+
+          </p><br>
+        </div>
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>
+      
+         
+  <?php  echo " Kholoud Mohamed contact  ";  ?>
+     
+
+
+</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p>
+      
+         
+  <?php  echo " search by  ";  ?>
+   <a href="https://www.google.com " target="_blank">
+
+
+       
+   <?php  echo " Google ";  ?>
+
+</a></p>
+</footer>
+
+</body>
+</html>
